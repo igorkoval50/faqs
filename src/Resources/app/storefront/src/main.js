7a9125be2cf1590bb1979df65d6ab31a375bcfeb
user <igor.k@ioksa.company>
2020-12-07 19:18:29 +0200

    style of accordion

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,11 +1,27 @@
-//smooth scrolling add
-$(".c108-faq-section-box a").click(function (e) {
-    e.preventDefault();
-    let elementClick = $(this).attr("href");
-    let destination = $(elementClick).offset().top - 80;
-    // window.scrollTo(0, destination);
-    scroll({
-        top: destination,
-        behavior: "smooth"
+$( document ).ready(function() {
+    function accrodionLink() {
+        let width = $( window ).width();
+        if (width > 576) {
+            $(".c108-faq-section-box a").click(function (e) {
+                e.preventDefault();
+                let elementClick = $(this).attr("href");
+                let destination = $(elementClick).offset().top - 80;
+                // window.scrollTo(0, destination);
+                scroll({
+                    top: destination,
+                    behavior: "smooth"
+                });
+            });
+        }
+        else {
+            $(".c108-faq-section-box a").click(function (e) {
+                e.preventDefault();
+            });
+        }
+    }
+    accrodionLink();
+    $( window ).resize();
+    $( window ).resize(function(){
+        accrodionLink();
     });
 });

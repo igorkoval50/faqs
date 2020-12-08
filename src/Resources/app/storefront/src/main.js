1da830d721fd3ff761b5e62a6dbd10d7afd6d6da
user <igor.k@ioksa.company>
2020-12-08 12:40:12 +0200

    finish mobile accordion

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,27 +1,12 @@
 $( document ).ready(function() {
-    function accrodionLink() {
-        let width = $( window ).width();
-        if (width > 576) {
-            $(".c108-faq-section-box a").click(function (e) {
-                e.preventDefault();
-                let elementClick = $(this).attr("href");
-                let destination = $(elementClick).offset().top - 80;
-                // window.scrollTo(0, destination);
-                scroll({
-                    top: destination,
-                    behavior: "smooth"
-                });
-            });
-        }
-        else {
-            $(".c108-faq-section-box a").click(function (e) {
-                e.preventDefault();
-            });
-        }
-    }
-    accrodionLink();
-    $( window ).resize();
-    $( window ).resize(function(){
-        accrodionLink();
+    $(".c108-faq-section-box a").click(function (e) {
+        e.preventDefault();
+        let elementClick = $(this).attr("href");
+        let destination = $(elementClick).offset().top - 80;
+        // window.scrollTo(0, destination);
+        scroll({
+            top: destination,
+            behavior: "smooth"
+        });
     });
 });
